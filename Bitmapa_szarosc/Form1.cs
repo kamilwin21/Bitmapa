cceb42e6a1b9a24a8f415ba3b6bf21a7a7d81f7a
kamilwin21 <kamilwin21@gmail.com>
2020-10-29 18:05:00 +0100

    Add new files

---
+++ Bitmapa_szarosc/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bitmapa_szarosc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+
+
+
+                openFileDialog.DefaultExt = ".png";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+
+                    // label1.Content = openFileDialog.FileName;
+                    // img1.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+
+                    Bitmap bm = new Bitmap(Image.FromFile(openFileDialog.FileName));
+                    Bitmap szary = new Bitmap(Image.FromFile(openFileDialog.FileName));
+                    int x, y, s;
+
+
+                    for (x = 0; x < bm.Width; x++)
+                    {
+                        for (y = 0; y < bm.Height; y++)
+                        {
+                            Color pixelColor = bm.GetPixel(x, y);
+
+                            s = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                            szary.SetPixel(x, y, Color.FromArgb(s, s, s));
+
+                            pictureBox1.Image = szary;
+
+
+                        }
+                    }
+
+
+
+                }
+
+            
+
+           
+        }
+    }
+}
